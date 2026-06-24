a58039b5e46d7ee18f90e60f87992167698357b6
greycsont <RuiD2019@outlook.com>
2026-06-24 06:38:04 +0100

    Changed behaviour of how Overlay works

--- src/UI/Overlay.cs
+++ src/UI/Overlay.cs
@@ -6,11 +6,8 @@ public abstract class Overlay : AnimatedPanel
 {
     public bool Visible { get; private set; }
 
-    public async UniTask Toggle()
-    {
-        if (Visible)
-            await Hide();
-        else
-            await Show();
-    }
+    public override UniTask Show() { Visible = true;  return base.Show(); }
+    public override UniTask Hide() { Visible = false; return base.Hide(); }
+
+    public UniTask Toggle() => Visible ? Hide() : Show();
 }
